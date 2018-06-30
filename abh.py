1b6b40e77947cd8b9edde75e5248fde9e0300767
xriser <xtmprz@gmail.com>
2018-06-30 23:10:28 +0300

    add python checker

---
+++ abh.py
@@ -0,0 +1,132 @@
+from influxdb import InfluxDBClient
+from influxdb.client import InfluxDBClientError
+import configparser
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+import time
+import re
+import requests
+import json
+
+#import sys
+
+
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+config = configparser.ConfigParser()
+config.read(dir_path + "/" + 'config.ini', encoding="utf8")
+sections = config.sections()
+
+InfluxDBHost     = config['settings']['InfluxDBHost']
+InfluxDBPort     = config['settings']['InfluxDBPort']
+InfluxDBUser     = config['settings']['InfluxDBUser']
+InfluxDBPass     = config['settings']['InfluxDBPass']
+InfluxDBDatabase = config['settings']['InfluxDBDatabase']
+
+BlynkUrl    = config.get('settings', 'BlynkUrl')
+BlynkPort   = config.get('settings', 'BlynkPort')
+BlynkToken  = config.get('settings', 'BlynkToken')
+
+
+def load_dirty_json(dirty_json):
+    regex_replace = [(r"([ \{,:\[])(u)?'([^']+)'", r'\1"\3"'), (r" False([, \}\]])", r' false\1'), (r" True([, \}\]])", r' true\1')]
+    for r, s in regex_replace:
+        dirty_json = re.sub(r, s, dirty_json)
+        clean_json = json.loads(dirty_json)
+    return clean_json
+
+def timer1():
+
+    #check energy today month
+    query = readInflux("select count(*) from abh where time > now() - 1h tz('Europe/Kiev')")
+
+    #print(std)
+    qdata = load_dirty_json(str(query.raw))
+    # print(qdata)
+
+    # list ['2018-06-30T21:34:07.839108789+03:00', 12, 12]
+    # print(qdata['series'][0]['values'][0])
+    list = qdata['series'][0]['values'][0]
+    # sum all values
+    sm = sum(list[1:len(list)])
+    print(sm)
+
+    #if sum all values = 0 - mean nobody home
+    if int(sm) > 0:
+        print('send to Blynk')
+        post_2blynk('V0', '1')
+
+
+
+
+    #stmdata = load_dirty_json(str(stm.raw))
+    #endata = load_dirty_json(str(end.raw))
+
+    #value_d = int(endata['series'][0]['values'][0][1]) - int(stdata['series'][0]['values'][0][1])
+    #value_m = int(endata['series'][0]['values'][0][1]) - int(stmdata['series'][0]['values'][0][1])
+
+    #post_2blynk('V9', str(value_d/1000))
+    #post_2blynk('V10', str(value_m/1000))
+
+
+
+
+def post_2blynk(pin, pin_value):
+    #print("Check response...")
+    try:
+        response = requests.get(BlynkUrl + ':' + BlynkPort + '/' + BlynkToken + '/update/' + pin + '?value='+ pin_value, timeout=(15, 15))
+        response.raise_for_status()
+
+    except requests.exceptions.ReadTimeout:
+        print('Oops. Read timeout occured')
+
+    except requests.exceptions.ConnectTimeout:
+        print('Oops. Connection timeout occured!')
+
+    except requests.exceptions.ConnectionError:
+        print('Seems like dns lookup failed..')
+
+    except requests.exceptions.HTTPError as err:
+        print('Oops. HTTP Error occured')
+        print('Response is: {content}'.format(content=err.response.content))
+
+    #print("Response status code: " + str(response.status_code))
+    text = response.text
+
+
+
+def readInflux(query):
+    #print(query)
+    result = ''
+    try:
+        client = InfluxDBClient(InfluxDBHost, InfluxDBPort, InfluxDBUser, InfluxDBPass, InfluxDBDatabase, timeout=10)
+    except Exception as err:
+        flash("Influx connection error: %s" % str(err))
+    if client:
+        #print("Read points:")
+        try:
+            result = client.query(query)
+        except Exception as err:
+            print('InfluxDBClientError = ' + str(err))
+    return result
+
+
+
+
+
+def main():
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(timer1, 'interval', seconds=5)
+    scheduler.start()
+
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
